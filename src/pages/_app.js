2908f2194733b4453939b90717e3904c9755c25f
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-06-18 18:36:31 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,21 @@
+// pages/_app.js
+
 import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <>
+  return (
     <ThemeProvider attribute="class">
+      <Head>
+        <link rel="icon" href="/MINE_LOGO.png" sizes="any"/>
+        <title>
+          Your Website Name
+        </title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </ThemeProvider>
-  </>;
+  );
 }
