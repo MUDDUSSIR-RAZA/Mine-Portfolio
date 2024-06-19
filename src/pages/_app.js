652b0ccfefe23bc8b428c68b12e4113851783734
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-06-19 22:16:29 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,9 +10,13 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Your Website Name</title>
       </Head>
-      <div className="min-h-screen dark:bg-[#030014] dark:overflow-y-scroll dark:overflow-x-hidden">
-        <Navbar />
-        <Hero />
+      <div className="dark:bg-[#030014] dark:overflow-y-hidden dark:overflow-x-hidden">
+        <div className="h-20 w-full">
+          <Navbar />
+        </div>
+        <div className="absolute top-0 right-0 left-0 mx-auto w-[900px] max-[960px]:w-[750px] max-[730px]:w-[550px] max-[550px]:w-[450px] h-[342px] max-[960px]:h-[298px] max-[730px]:h-[239px] max-[550px]:h-[212px] z-0">
+          <Hero />
+        </div>
         <Component {...pageProps} />
       </div>
     </ThemeProvider>
